f0323ead0ce2bae0e677f3420fa8928a9178b75c
mokker <amo0314@163.com>
2022-11-03 21:25:44 +0800

    install tailwind.css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import './assets/style/tailwind.css'
+
 import { Button, NavBar } from 'vant'
 
 const app = createApp(App)
