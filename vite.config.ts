ac05be254c1689c52a95dc4e245605d3a23d12c0
mokker <amo0314@163.com>
2023-03-04 14:11:09 +0800

    路径解析配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import styleImport, { VantResolve } from 'vite-plugin-style-import';
+import { resolve } from 'path' 
+ 
+const pathResolve = (dir: string): any => {  
+  return resolve(__dirname, ".", dir)          
+}
+ 
+const alias: Record<string, string> = {
+  '@': pathResolve("src")
+}
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +26,8 @@ export default defineConfig({
         }
       ]
     }),
-  ]
+  ],
+  resolve: {
+    alias
+  }
 })
